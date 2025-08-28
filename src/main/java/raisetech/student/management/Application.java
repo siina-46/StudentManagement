fee6001563048ba22600efdcb12e2b5b9a676d96
Shina <sii3112@outlook.jp>
2025-08-29 01:51:42 +0900

    commitとpush

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -3,18 +3,33 @@ package raisetech.student.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+  private String name = "Hamada Shiina";
+  private String age = "29";
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello,World";
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
+  }
+
+  @PostMapping("/studentInfo")
+  public void  setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
   }
 }
