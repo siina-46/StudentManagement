83473ff8d6ae12de64aba9547c4a162073caf036
Shina <sii3112@outlook.jp>
2025-08-30 04:58:38 +0900

    Gitの理解不足だけど保存する目的でプッシュとコミットをした。

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,35 +1,44 @@
 package raisetech.student.management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-  private String name = "Hamada Shiina";
-  private String age = "29";
+  @Autowired
+  private  StudentRepository repository;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @GetMapping("/studentInfo")
-  public String getStudentInfo() {
-    return name + " " + age + "歳";
+  @GetMapping("/student")
+  public String getStudent(@RequestParam String name) {
+    Student student = repository.searchByName(name);
+    return student.getName()+ " " + student.getAge() + "歳";
   }
 
-  @PostMapping("/studentInfo")
-  public void  setStudentInfo(String name, String age) {
-    this.name = name;
-    this.age = age;
+  @PostMapping("/student")
+  public void  registerStudent(String name, int age) {
+    repository.registerStudent(name,age);
   }
 
-  @PostMapping("/studentName")
-  public void updateStudentName(String name) {
-    this.name = name;
+  @PatchMapping("/student")
+  public void updateStudentName(String name, int age) {
+    repository.updateStudent(name, age);
+  }
+
+  @DeleteMapping("/student")
+  public void deleteStudent(String name) {
+    repository.deleteStudent(name);
   }
 }
